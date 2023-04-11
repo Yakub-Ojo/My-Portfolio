4589c9cbf14ab500af14160141f7f4e31a223451
kessie <kayp3477@gmail.com>
2023-04-11 11:23:06 +0000

    Close menu when clicked

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ const menu = document.querySelector(".nav-list");
 const overlay = document.querySelector(".overlay");
 const body = document.body;
 
-menuBtn.addEventListener('click', function () {
-  closeBtn.style.display = 'block';
+menuBtn.addEventListener("click", function () {
+  closeBtn.style.display = "block";
   menuBtn.style.display = "none";
-  menu.style.display = 'Flex';
+  menu.style.display = "Flex";
   overlay.classList.remove("hidden");
   body.style.overflow = "hidden";
-})
+});
 
 const closeMenu = () => {
-  menuBtn.style.display = 'block';
-  closeBtn.style.display = 'none';
-  menu.style.display = 'none'
-  overlay.classList.add('hidden');
-  body.style.overflow = 'scroll'
-}
-    
-closeBtn.addEventListener('click', function () {
+  menuBtn.style.display = "block";
+  closeBtn.style.display = "none";
+  menu.style.display = "none";
+  overlay.classList.add("hidden");
+  body.style.overflow = "scroll";
+};
+
+closeBtn.addEventListener("click", function () {
   closeMenu();
-})    
+});
+
+const navItems = document.querySelectorAll(".nav-list a");
+if (window.innerWidth < 1000) {
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      closeMenu();
+    });
+  });
+}
