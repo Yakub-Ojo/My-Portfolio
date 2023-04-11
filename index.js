f8cae3adab2dc82a5c47d2dc22784a09fd3a01be
Yakub Ojo <yakubojodev@gmail.com>
2023-04-11 12:03:13 +0100

    Add event listener to the hamburger menu

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector(".menu-icon");
+const closeBtn = document.querySelector(".close_icon");
+const menu = document.querySelector(".nav-list");
+console.log(menu)
+const overlay = document.querySelector(".overlay");
+const body = document.body;
+
 menuBtn.addEventListener('click', function () {
   closeBtn.style.display = 'block';
   menuBtn.style.display = "none";
@@ -5,3 +12,4 @@ menuBtn.addEventListener('click', function () {
   overlay.classList.remove("hidden");
   body.style.overflow = "hidden"
   })
+
